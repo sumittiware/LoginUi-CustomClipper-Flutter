5117bf2047c10fa5ef484eb73b7a2493e4b2d3dd
Sumit Tiware <64718675+sumittiware@users.noreply.github.com>
2021-02-15 15:47:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MyShop',
+      title: 'Login UI',
       theme: ThemeData(
           primaryColor: blue, accentColor: bgColor, fontFamily: 'NotoSansJP'),
       home: SignInPage(),
